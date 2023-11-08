c4825e10cd7112bf6bb83109f3f2a3256f6dac2d
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-08 12:42:28 -0500

    tyver

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Ernest
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+//variables
+const servoNumberOne = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servoNumberOne, 0)
+  basic.clearScreen()
+  basic.showString('0')
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servoNumberOne, 180)
+  basic.clearScreen()
+  basic.showString('180')
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
