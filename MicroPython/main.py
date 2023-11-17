c3883e32c42268798ff390e54007c57516db37f1
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-17 18:15:32 +0000

    pyyoh ver

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,64 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ernest
+Created on: Nov 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from math import *
 
 
-display.scroll("Hello, World!")
+class Servo:
+    def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
+        self.min_us = min_us
+        self.max_us = max_us
+        self.us = 0
+        self.freq = freq
+        self.angle = angle
+        self.analog_period = 0
+        self.pin = pin
+        analog_period = round((1 / self.freq) * 1000)  # hertz to miliseconds
+        self.pin.set_analog_period(analog_period)
+
+    def write_us(self, us):
+        us = min(self.max_us, max(self.min_us, us))
+        duty = round(us * 1024 * self.freq // 1000000)
+        self.pin.write_analog(duty)
+        sleep(1000)
+        self.pin.write_digital(0)  # turn the pin off
+
+    def write_angle(self, degrees=None):
+        if degrees is None:
+            degrees = math.degrees(radians)
+        degrees = degrees % 360
+        total_range = self.max_us - self.min_us
+        us = self.min_us + total_range * degrees // self.angle
+        self.write_us(us)
+
+
+# setup
+display.clear()
+display.show(Image.HAPPY)
+
+# loop
+while True:
+    if button_a.is_pressed():
+        # turn servo to 0 degrees
+        Servo(pin14).write_angle(0)
+        display.clear()
+        display.show("0")
+        sleep(500)
+        display.show(Image.SQUARE_SMALL)
+        sleep(500)
+    display.clear()
+    display.show(Image.HAPPY)
+
+    if button_b.is_pressed():
+        # turn servo to 180 degrees
+        Servo(pin14).write_angle(180)
+        display.clear()
+        display.show("180") 
+        sleep(500)
+        display.show(Image.SQUARE_SMALL)
+        sleep(500)
+    display.show(Image.HAPPY)
